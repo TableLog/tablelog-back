929f3177d597c66194a76a0e1ffd78be3dd99800
Euna Jeong <kysn75@gmail.com>
2025-03-28 10:57:26 +0900

    refactor: Food 조회 시 API 명세서 내용 수정

--- src/main/java/com/tablelog/tablelogback/domain/food/controller/FoodController.java
+++ src/main/java/com/tablelog/tablelogback/domain/food/controller/FoodController.java
@@ -6,6 +6,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,15 +26,33 @@ public class FoodController {
     }
 
     @GetMapping("/foods/{foodId}")
-    public void readFood(
+    public Map<String, Object> readFood(
             @PathVariable Long foodId
     ){
-
+        Map<String, Object> food1 = new HashMap<>();
+        food1.put("name", "사과");
+        food1.put("foodUnit", "g");
+        food1.put("cal", 300);
+        food1.put("user", "user1");
+        return food1;
     }
 
     @GetMapping("/foods")
-    public void readAllFoods(){
-
+    public List<Map<String, Object>> readAllFoods(){
+        List<Map<String, Object>> foods = new ArrayList<>();
+        Map<String, Object> food1 = new HashMap<>();
+        food1.put("name", "사과");
+        food1.put("foodUnit", "g");
+        food1.put("cal", 300);
+        food1.put("user", "user1");
+        Map<String, Object> food2 = new HashMap<>();
+        food2.put("name", "사과");
+        food2.put("foodUnit", "g");
+        food2.put("cal", 300);
+        food2.put("user", "user1");
+        foods.add(food1);;
+        foods.add(food2);
+        return foods;
     }
 
     @PutMapping("/foods/{foodId}")
