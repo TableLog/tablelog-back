ce19745f0c6cfc771cc1fb401549d9a1df11727e
Euna Jeong <kysn75@gmail.com>
2025-03-28 10:04:23 +0900

    feature: Recipe 생성 시 RecipeFood와 RecipeProcess 리스트 생성 기능 추가

--- src/main/java/com/tablelog/tablelogback/domain/recipe/controller/RecipeController.java
+++ src/main/java/com/tablelog/tablelogback/domain/recipe/controller/RecipeController.java
@@ -1,5 +1,7 @@
 package com.tablelog.tablelogback.domain.recipe.controller;
 
+import com.tablelog.tablelogback.domain.recipe_food.controller.dto.controller.RecipeFoodCreateControllerRequestDto;
+import com.tablelog.tablelogback.domain.recipe_process.controller.dto.controller.RecipeProcessCreateControllerRequestDto;
 import com.tablelog.tablelogback.global.enums.RecipeCategory;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -27,7 +30,10 @@ public class RecipeController {
             Byte star,
             Integer price,
             String memo,
-            MultipartFile multipartFile
+            MultipartFile recipeImage,
+            List<RecipeProcessCreateControllerRequestDto> RecipeProcessCreateControllerRequestDto,
+            List<MultipartFile> recipeProcessImage,
+            List<RecipeFoodCreateControllerRequestDto> RecipeFoodCreateControllerRequestDto
 //            @Valid @RequestPart(name = "recipeCreateRequestDto")
 //            @RequestBody RecipeCreateControllerRequestDto controllerRequestDto
 //            @RequestPart(name = "recipeCreateImage",required = false) MultipartFile recipeImage,
---
+++ src/main/java/com/tablelog/tablelogback/domain/recipe_process/controller/dto/controller/RecipeProcessCreateControllerRequestDto.java
@@ -0,0 +1,6 @@
+package com.tablelog.tablelogback.domain.recipe_process.controller.dto.controller;
+
+public record RecipeProcessCreateControllerRequestDto(
+
+) {
+}
