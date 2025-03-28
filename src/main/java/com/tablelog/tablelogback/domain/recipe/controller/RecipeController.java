aed745c42585466d2ed6fe2d7cfd07eff72c3d41
Euna Jeong <kysn75@gmail.com>
2025-03-28 10:58:53 +0900

    refactor: Recipe 조회 시 API 명세서 내용 수정

--- src/main/java/com/tablelog/tablelogback/domain/recipe/controller/RecipeController.java
+++ src/main/java/com/tablelog/tablelogback/domain/recipe/controller/RecipeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -46,20 +46,159 @@ public class RecipeController {
     }
 
     @GetMapping("/recipes/{recipeId}")
-    public void readRecipe(
+    public Map<String, Object> readRecipe(
             @PathVariable Long recipeId
     ){
+        Map<String, Object> recipe = new HashMap<>();
+        recipe.put("title", "레시피 제목");
+        recipe.put("intro", "간단한 설명");
+        recipe.put("state", 1);
+        recipe.put("recipeCategory", "한식");
+        recipe.put("star", 2);
+        recipe.put("price", 10000);
+        recipe.put("memo", "메모");
+        recipe.put("image_url", "https://example.com/image1.jpg");
 
+        // 레시피 조리과정 리스트 생성
+        List<Map<String, Object>> recipe_processes = new ArrayList<>();
+        Map<String, Object> recipe_process1 = new HashMap<>();
+        recipe_process1.put("sequence", 1);
+        recipe_process1.put("description", "나나나");
+        recipe_process1.put("image_url", "https://example.com/image1.jpg");
+        recipe_process1.put("user", "user");
+        Map<String, Object> recipe_process2 = new HashMap<>();
+        recipe_process2.put("sequence", 2);
+        recipe_process2.put("description", "나나나");
+        recipe_process2.put("image_url", "https://example.com/image1.jpg");
+        recipe_process2.put("user", "user");
+        recipe_processes.add(recipe_process1);
+        recipe_processes.add(recipe_process2);
+        recipe.put("recipe_processes", recipe_processes);
+
+        List<MultipartFile> recipeProcessImages = new LinkedList<>();
+        recipe.put("recipeProcessImages", recipeProcessImages);
+
+        // 레시피 식재료 리스트 생성
+        List<Map<String, Object>> recipe_foods = new ArrayList<>();
+        Map<String, Object> recipe_food1 = new HashMap<>();
+        recipe_food1.put("foodAmount", "ts");
+        recipe_food1.put("foodName", "사과");
+        recipe_food1.put("user", "user1");
+        Map<String, Object> recipe_food2 = new HashMap<>();
+        recipe_food2.put("foodAmount", "ts");
+        recipe_food2.put("foodName", "사과");
+        recipe_food2.put("user", "user1");
+        recipe_foods.add(recipe_food1);;
+        recipe_foods.add(recipe_food2);
+        recipe.put("recipe_foods", recipe_foods);
+
+        recipe.put("user", "user1");
+        return recipe;
     }
 
     @GetMapping("/recipes")
-    public void readAllRecipes(){
+    public List<Map<String, Object>> readAllRecipes(){
+        List<Map<String, Object>> recipes = new ArrayList<>();
 
+        Map<String, Object> recipe = new HashMap<>();
+        recipe.put("title", "레시피 제목");
+        recipe.put("intro", "간단한 설명");
+        recipe.put("state", 1);
+        recipe.put("recipeCategory", "한식");
+        recipe.put("star", 2);
+        recipe.put("price", 10000);
+        recipe.put("memo", "메모");
+        recipe.put("image_url", "https://example.com/image1.jpg");
+
+        // 레시피 조리과정 리스트 생성
+        List<Map<String, Object>> recipe_processes = new ArrayList<>();
+        Map<String, Object> recipe_process1 = new HashMap<>();
+        recipe_process1.put("sequence", 1);
+        recipe_process1.put("description", "나나나");
+        recipe_process1.put("image_url", "https://example.com/image1.jpg");
+        recipe_process1.put("user", "user");
+        Map<String, Object> recipe_process2 = new HashMap<>();
+        recipe_process2.put("sequence", 2);
+        recipe_process2.put("description", "나나나");
+        recipe_process2.put("image_url", "https://example.com/image1.jpg");
+        recipe_process2.put("user", "user");
+        recipe_processes.add(recipe_process1);
+        recipe_processes.add(recipe_process2);
+        recipe.put("recipe_processes", recipe_processes);
+
+        List<MultipartFile> recipeProcessImages = new LinkedList<>();
+        recipe.put("recipeProcessImages", recipeProcessImages);
+
+        // 레시피 식재료 리스트 생성
+        List<Map<String, Object>> recipe_foods = new ArrayList<>();
+        Map<String, Object> recipe_food1 = new HashMap<>();
+        recipe_food1.put("foodAmount", "ts");
+        recipe_food1.put("foodName", "사과");
+        recipe_food1.put("user", "user1");
+        Map<String, Object> recipe_food2 = new HashMap<>();
+        recipe_food2.put("foodAmount", "ts");
+        recipe_food2.put("foodName", "사과");
+        recipe_food2.put("user", "user1");
+        recipe_foods.add(recipe_food1);;
+        recipe_foods.add(recipe_food2);
+        recipe.put("recipe_foods", recipe_foods);
+
+        recipe.put("user", "user1");
+
+        recipes.add(recipe);
+        return recipes;
     }
 
     @GetMapping("/users/{userId}/recipes")
-    public void readAllRecipesByUser(){
+    public List<Map<String, Object>> readAllRecipesByUser(){
+        List<Map<String, Object>> recipes = new ArrayList<>();
+        Map<String, Object> recipe = new HashMap<>();
+        recipe.put("title", "레시피 제목");
+        recipe.put("intro", "간단한 설명");
+        recipe.put("state", 1);
+        recipe.put("recipeCategory", "한식");
+        recipe.put("star", 2);
+        recipe.put("price", 10000);
+        recipe.put("memo", "메모");
+        recipe.put("image_url", "https://example.com/image1.jpg");
 
+        // 레시피 조리과정 리스트 생성
+        List<Map<String, Object>> recipe_processes = new ArrayList<>();
+        Map<String, Object> recipe_process1 = new HashMap<>();
+        recipe_process1.put("sequence", 1);
+        recipe_process1.put("description", "나나나");
+        recipe_process1.put("image_url", "https://example.com/image1.jpg");
+        recipe_process1.put("user", "user");
+        Map<String, Object> recipe_process2 = new HashMap<>();
+        recipe_process2.put("sequence", 2);
+        recipe_process2.put("description", "나나나");
+        recipe_process2.put("image_url", "https://example.com/image1.jpg");
+        recipe_process2.put("user", "user");
+        recipe_processes.add(recipe_process1);
+        recipe_processes.add(recipe_process2);
+        recipe.put("recipe_processes", recipe_processes);
+
+        List<MultipartFile> recipeProcessImages = new LinkedList<>();
+        recipe.put("recipeProcessImages", recipeProcessImages);
+
+        // 레시피 식재료 리스트 생성
+        List<Map<String, Object>> recipe_foods = new ArrayList<>();
+        Map<String, Object> recipe_food1 = new HashMap<>();
+        recipe_food1.put("foodAmount", "ts");
+        recipe_food1.put("foodName", "사과");
+        recipe_food1.put("user", "user1");
+        Map<String, Object> recipe_food2 = new HashMap<>();
+        recipe_food2.put("foodAmount", "ts");
+        recipe_food2.put("foodName", "사과");
+        recipe_food2.put("user", "user1");
+        recipe_foods.add(recipe_food1);;
+        recipe_foods.add(recipe_food2);
+        recipe.put("recipe_foods", recipe_foods);
+
+        recipe.put("user", "user1");
+
+        recipes.add(recipe);
+        return recipes;
     }
 
     @PutMapping("/recipes/{recipeId}")
