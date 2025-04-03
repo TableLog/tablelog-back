81592099a43ebb5be53130cbb59ebeddd922de2c
Euna Jeong <kysn75@gmail.com>
2025-04-03 11:47:28 +0900

    feature: 회원가입을 위한 Dto 생성

---
+++ src/main/java/com/tablelog/tablelogback/domain/user/dto/controller/UserSignUpControllerRequestDto.java
@@ -0,0 +1,9 @@
+package com.tablelog.tablelogback.domain.user.dto.controller;
+
+public record UserSignUpControllerRequestDto(
+        String email,
+        String password,
+        String confirmPassword,
+        String nickname
+) {
+}
---
+++ src/main/java/com/tablelog/tablelogback/domain/user/dto/service/request/UserSignUpServiceRequestDto.java
@@ -0,0 +1,9 @@
+package com.tablelog.tablelogback.domain.user.dto.service.request;
+
+public record UserSignUpServiceRequestDto(
+        String email,
+        String password,
+        String confirmPassword,
+        String nickname
+) {
+}
