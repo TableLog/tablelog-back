f22778692bc9fa62325309963706752154a2724c
Euna Jeong <kysn75@gmail.com>
2025-04-06 18:35:57 +0900

    feature: User exception에 존재하는 사용자, 카카오 실패, notfound 카카오 exception 생성

--- src/main/java/com/tablelog/tablelogback/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/tablelog/tablelogback/domain/user/exception/UserErrorCode.java
@@ -9,13 +9,16 @@ import org.springframework.http.HttpStatus;
 public enum UserErrorCode implements ErrorCode {
     // 400
     ALREADY_EXIST_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다."),
+    ALREADY_EXIST_USER(HttpStatus.BAD_REQUEST, "이미 존재하는 사용자입니다."),
     DUPLICATE_NICKNAME(HttpStatus.BAD_REQUEST, "중복된 닉네임입니다."),
     NOT_MATCH_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
     MATCH_CURRENT_PASSWORD(HttpStatus.BAD_REQUEST, "기존 비밀번호입니다."),
     BAD_LOGIN(HttpStatus.BAD_REQUEST, "로그인에 실패하였습니다."),
+    FAILED_UNLINK_KAKAO(HttpStatus.BAD_REQUEST, "카카오 동의 철회에 실패하였습니다."),
 
     // 404
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+    NOT_FOUND_KAKAO_USER(HttpStatus.NOT_FOUND, "사용자 카카오 계정을 찾을 수 없습니다."),
     NOT_ADMIN(HttpStatus.FORBIDDEN, "권한이 없습니다.");
 
     private final HttpStatus status;
