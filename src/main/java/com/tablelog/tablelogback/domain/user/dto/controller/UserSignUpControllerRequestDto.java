ec4718ed6ed98c6cb0de035158116ca56b67319d
Euna Jeong <kysn75@gmail.com>
2025-04-21 02:12:21 +0900

    refactor: User 회원가입 dto에 provider, imgurl 추가

--- src/main/java/com/tablelog/tablelogback/domain/user/dto/controller/UserSignUpControllerRequestDto.java
+++ src/main/java/com/tablelog/tablelogback/domain/user/dto/controller/UserSignUpControllerRequestDto.java
@@ -1,11 +1,15 @@
 package com.tablelog.tablelogback.domain.user.dto.controller;
 
+import com.tablelog.tablelogback.global.enums.UserProvider;
+
 public record UserSignUpControllerRequestDto(
         String email,
         String password,
         String confirmPassword,
         String nickname,
         String name,
-        String birthday
+        String birthday,
+        UserProvider provider,
+        String imgUrl
 ) {
 }
