415427cb7c77489e331e9dab4b88816bf28cdc05
Euna Jeong <kysn75@gmail.com>
2025-05-31 22:25:52 +0900

    refactor: Recipe 카테고리 오류 수정

--- src/main/java/com/tablelog/tablelogback/domain/recipe/dto/controller/RecipeUpdateControllerRequestDto.java
+++ src/main/java/com/tablelog/tablelogback/domain/recipe/dto/controller/RecipeUpdateControllerRequestDto.java
@@ -3,12 +3,15 @@ package com.tablelog.tablelogback.domain.recipe.dto.controller;
 import com.tablelog.tablelogback.global.enums.CookingTime;
 import com.tablelog.tablelogback.global.enums.RecipeCategory;
 import com.tablelog.tablelogback.global.enums.RecipePrice;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
 public record RecipeUpdateControllerRequestDto(
         String title,
         String intro,
+        @ArraySchema(schema = @Schema(implementation = RecipeCategory.class))
         List<RecipeCategory> recipeCategoryList,
         RecipePrice price,
         CookingTime cookingTime,
--- src/main/java/com/tablelog/tablelogback/domain/recipe/entity/Recipe.java
+++ src/main/java/com/tablelog/tablelogback/domain/recipe/entity/Recipe.java
@@ -39,7 +39,7 @@ public class Recipe extends BaseEntity {
     @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "recipe_recipe_category_list", joinColumns = @JoinColumn(name = "recipe_id"))
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(name = "category", nullable = false)
     private List<RecipeCategory> recipeCategoryList;
 
     @Column(nullable = false)
