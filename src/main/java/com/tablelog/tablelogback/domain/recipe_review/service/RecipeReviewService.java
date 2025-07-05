5536eeeb7c3a5b21bca1e0a835343a0fbade6a42
Euna Jeong <kysn75@gmail.com>
2025-07-05 21:43:53 +0900

    refactor: RecipeReview 댓글, 답글 생성 api 분리

--- src/main/java/com/tablelog/tablelogback/domain/recipe_review/service/RecipeReviewService.java
+++ src/main/java/com/tablelog/tablelogback/domain/recipe_review/service/RecipeReviewService.java
@@ -1,14 +1,12 @@
 package com.tablelog.tablelogback.domain.recipe_review.service;
 
-import com.tablelog.tablelogback.domain.recipe_review.dto.service.RecipeReviewCreateServiceRequestDto;
-import com.tablelog.tablelogback.domain.recipe_review.dto.service.RecipeReviewReadResponseDto;
-import com.tablelog.tablelogback.domain.recipe_review.dto.service.RecipeReviewSliceResponseDto;
-import com.tablelog.tablelogback.domain.recipe_review.dto.service.RecipeReviewUpdateServiceRequestDto;
+import com.tablelog.tablelogback.domain.recipe_review.dto.service.*;
 import com.tablelog.tablelogback.domain.user.entity.User;
 import com.tablelog.tablelogback.global.security.UserDetailsImpl;
 
 public interface RecipeReviewService {
     void createRecipeReview(RecipeReviewCreateServiceRequestDto serviceRequestDto, Long recipeId, User user);
+    void createRecipeReply(RecipeReviewReplyCreateServiceRequestDto serviceRequestDto, Long recipeId, User user);
     RecipeReviewReadResponseDto readRecipeReview(Long recipeId, Long id, UserDetailsImpl userDetails);
     RecipeReviewSliceResponseDto readAllRecipeReviewsByRecipe(Long recipeId, int pageNumber, UserDetailsImpl userDetails);
     RecipeReviewSliceResponseDto readAllRecipeReviewsByUser(Long userId, int pageNumber, UserDetailsImpl userDetails);
--- src/main/java/com/tablelog/tablelogback/domain/recipe_review/service/impl/RecipeReviewServiceImpl.java
+++ src/main/java/com/tablelog/tablelogback/domain/recipe_review/service/impl/RecipeReviewServiceImpl.java
@@ -4,10 +4,7 @@ import com.tablelog.tablelogback.domain.recipe.entity.Recipe;
 import com.tablelog.tablelogback.domain.recipe.exception.NotFoundRecipeException;
 import com.tablelog.tablelogback.domain.recipe.exception.RecipeErrorCode;
 import com.tablelog.tablelogback.domain.recipe.repository.RecipeRepository;
-import com.tablelog.tablelogback.domain.recipe_review.dto.service.RecipeReviewCreateServiceRequestDto;
-import com.tablelog.tablelogback.domain.recipe_review.dto.service.RecipeReviewReadResponseDto;
-import com.tablelog.tablelogback.domain.recipe_review.dto.service.RecipeReviewSliceResponseDto;
-import com.tablelog.tablelogback.domain.recipe_review.dto.service.RecipeReviewUpdateServiceRequestDto;
+import com.tablelog.tablelogback.domain.recipe_review.dto.service.*;
 import com.tablelog.tablelogback.domain.recipe_review.entity.RecipeReview;
 import com.tablelog.tablelogback.domain.recipe_review.exception.ForbiddenAccessRecipeReviewException;
 import com.tablelog.tablelogback.domain.recipe_review.exception.NotFoundRecipeReviewException;
@@ -44,21 +41,23 @@ public class RecipeReviewServiceImpl implements RecipeReviewService {
     public void createRecipeReview(RecipeReviewCreateServiceRequestDto serviceRequestDto, Long recipeId, User user){
         Recipe recipe = recipeRepository.findById(recipeId)
                 .orElseThrow(() -> new NotFoundRecipeException(RecipeErrorCode.NOT_FOUND_RECIPE));
+        RecipeReview recipeReview = recipeReviewEntityMapper.toRecipeReview(serviceRequestDto, recipeId, user, 0L);
+        recipeReviewRepository.save(recipeReview);
+        recipe.updateReviewCount(recipe.getReviewCount() + 1);
+        recipe.addStar(serviceRequestDto.star());
+        user.addPointBalance(100);
+    }
+
+    @Transactional
+    public void createRecipeReply(RecipeReviewReplyCreateServiceRequestDto serviceRequestDto, Long recipeId, User user){
+        Recipe recipe = recipeRepository.findById(recipeId)
+                .orElseThrow(() -> new NotFoundRecipeException(RecipeErrorCode.NOT_FOUND_RECIPE));
         // 대댓글은 작성자만 한 개만 가능
-        if(serviceRequestDto.prrId() != 0){
-            if(recipeReviewRepository.existsByPrrId(serviceRequestDto.prrId()) || !isRecipeAuthorOrAdmin(recipe, user)){
-                throw new ForbiddenAccessRecipeReviewException(RecipeReviewErrorCode.FORBIDDEN_ACCESS_RECIPE_REVIEW);
-            }
-            RecipeReview recipeReview = recipeReviewEntityMapper.toRecipeReview(serviceRequestDto, recipeId, user);
-            recipeReviewRepository.save(recipeReview);
+        if(recipeReviewRepository.existsByPrrId(serviceRequestDto.prrId()) || !isRecipeAuthorOrAdmin(recipe, user)){
+            throw new ForbiddenAccessRecipeReviewException(RecipeReviewErrorCode.FORBIDDEN_ACCESS_RECIPE_REVIEW);
         }
-        else {
-            RecipeReview recipeReview = recipeReviewEntityMapper.toRecipeReview(serviceRequestDto, recipeId, user);
-            recipeReviewRepository.save(recipeReview);
-            recipe.updateReviewCount(recipe.getReviewCount() + 1);
-            recipe.addStar(serviceRequestDto.star());
-            user.addPointBalance(100);
-        }
+        RecipeReview recipeReview = recipeReviewEntityMapper.toRecipeReply(serviceRequestDto, recipeId, user);
+        recipeReviewRepository.save(recipeReview);
     }
 
     @Override
@@ -77,7 +76,8 @@ public class RecipeReviewServiceImpl implements RecipeReviewService {
 
     @Override
     public RecipeReviewSliceResponseDto readAllRecipeReviewsByRecipe(
-            Long recipeId, int pageNumber, UserDetailsImpl userDetails) {
+            Long recipeId, int pageNumber, UserDetailsImpl userDetails
+    ) {
         Recipe recipe = recipeRepository.findById(recipeId)
                 .orElseThrow(() -> new NotFoundRecipeException(RecipeErrorCode.NOT_FOUND_RECIPE));
         PageRequest pageRequest = PageRequest.of(pageNumber, 5, Sort.by(Sort.Direction.DESC, "id"));
