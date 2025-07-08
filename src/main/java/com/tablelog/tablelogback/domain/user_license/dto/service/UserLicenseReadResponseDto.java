fce873a19b86dbff14e22235e2244e7a9a654832
Euna Jeong <kysn75@gmail.com>
2025-07-08 11:34:04 +0900

    refactor: RecipeReview get 시 profileImgUrl, isWriter 추가

--- src/main/java/com/tablelog/tablelogback/domain/user_license/dto/service/UserLicenseReadResponseDto.java
+++ src/main/java/com/tablelog/tablelogback/domain/user_license/dto/service/UserLicenseReadResponseDto.java
@@ -1,7 +1,7 @@
 package com.tablelog.tablelogback.domain.user_license.dto.service;
 
+import com.tablelog.tablelogback.global.enums.ApplyStatus;
 import com.tablelog.tablelogback.global.enums.LicenseType;
-import com.tablelog.tablelogback.global.enums.RequestStatus;
 
 import java.time.LocalDateTime;
 
@@ -10,7 +10,7 @@ public record UserLicenseReadResponseDto(
         Long userId,
         String licenseName,
         LicenseType licenseType,
-        RequestStatus status,
+        ApplyStatus status,
         LocalDateTime modifiedAt
 ) {
 }
