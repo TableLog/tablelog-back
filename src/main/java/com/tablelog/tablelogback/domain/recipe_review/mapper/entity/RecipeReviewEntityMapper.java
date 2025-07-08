d007bd87b9bc71f7f2d49936dc77c3941a98d5d3
Euna Jeong <kysn75@gmail.com>
2025-07-08 11:30:53 +0900

    refactor: RecipeReview 댓글, 답글 분리 생성 위한 설정

--- src/main/java/com/tablelog/tablelogback/domain/recipe_review/mapper/entity/RecipeReviewEntityMapper.java
+++ src/main/java/com/tablelog/tablelogback/domain/recipe_review/mapper/entity/RecipeReviewEntityMapper.java
@@ -16,13 +16,36 @@ public interface RecipeReviewEntityMapper {
     @Mapping(source = "user.nickname", target = "user")
     @Mapping(source = "recipeId", target = "recipeId")
     @Mapping(source = "prrId", target = "prrId")
-    RecipeReview toRecipeReview(RecipeReviewCreateServiceRequestDto serviceRequestDto, Long recipeId, User user, Long prrId);
+    RecipeReview toRecipeReview(RecipeReviewCreateServiceRequestDto serviceRequestDto,
+                                Long recipeId, User user, Long prrId);
 
     @Mapping(source = "user.nickname", target = "user")
     @Mapping(source = "recipeId", target = "recipeId")
     RecipeReview toRecipeReply(RecipeReviewReplyCreateServiceRequestDto serviceRequestDto, Long recipeId, User user);
 
-    RecipeReviewReadResponseDto toRecipeReviewReadResponseDto(RecipeReview recipeReview, Boolean isReviewer, Boolean isWriter);
+    RecipeReviewReadResponseDto toRecipeReviewReadResponseDto(
+            RecipeReview recipeReview, Boolean isReviewer, String profileImgUrl);
+
+    default RecipeReviewReadResponseDto toDtoWithReply(
+            RecipeReview recipeReview,
+            boolean isReviewer,
+            String profileImgUrl,
+            RecipeReviewReadResponseDto reply
+    ) {
+        RecipeReviewReadResponseDto base = toRecipeReviewReadResponseDto(recipeReview, isReviewer, profileImgUrl);
+        return new RecipeReviewReadResponseDto(
+                base.id(),
+                base.content(),
+                base.star(),
+                base.recipeId(),
+                base.user(),
+                base.modifiedAt(),
+                base.prrId(),
+                base.isReviewer(),
+                base.profileImgUrl(),
+                reply
+        );
+    }
 
     @Mapping(source = "modifiedAt", target = "modifiedAt")
     List<RecipeReviewReadResponseDto> toRecipeReviewReadAllResponseDtoLists(List<RecipeReview> recipeReviewList);
