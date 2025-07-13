0cfb053fd44eb82f086bde76cf3df6d7c0906667
Euna Jeong <kysn75@gmail.com>
2025-07-13 14:24:56 +0900

    feature: PointReason enum 생성

---
+++ src/main/java/com/tablelog/tablelogback/global/enums/PointReason.java
@@ -0,0 +1,15 @@
+package com.tablelog.tablelogback.global.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+public enum PointReason {
+    회원가입,
+    레시피등록,
+    피드등록,
+    레시피구매;
+
+    @JsonCreator
+    public static PointReason fromString(String inputValue) {
+        return EnumUtils.fromString(PointReason.class, inputValue);
+    }
+}
