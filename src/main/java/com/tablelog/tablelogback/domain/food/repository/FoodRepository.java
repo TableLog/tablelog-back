bc6ce698fac828776c2264df0601d23531ea5eea
Euna Jeong <kysn75@gmail.com>
2025-11-09 10:38:53 +0900

    refactor: Food 불필요한 import 삭제

--- src/main/java/com/tablelog/tablelogback/domain/food/repository/FoodRepository.java
+++ src/main/java/com/tablelog/tablelogback/domain/food/repository/FoodRepository.java
@@ -5,11 +5,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface FoodRepository extends JpaRepository<Food, Long> {
     boolean existsByFoodName(String foodName);
-    Slice<Food> findAllBy(Pageable pageable);
-    List<Food> findAllByFoodNameContaining(String foodName);
     Slice<Food> findByFoodNameContaining(String foodName, Pageable pageable);
 }
