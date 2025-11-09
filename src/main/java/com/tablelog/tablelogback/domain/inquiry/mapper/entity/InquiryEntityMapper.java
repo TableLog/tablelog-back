ae60b49021f4960485c52ea139bca16b88480e28
Euna Jeong <kysn75@gmail.com>
2025-11-09 10:39:17 +0900

    refactor: Inquiry 불필요한 import 삭제

--- src/main/java/com/tablelog/tablelogback/domain/inquiry/mapper/entity/InquiryEntityMapper.java
+++ src/main/java/com/tablelog/tablelogback/domain/inquiry/mapper/entity/InquiryEntityMapper.java
@@ -2,7 +2,6 @@ package com.tablelog.tablelogback.domain.inquiry.mapper.entity;
 
 import com.tablelog.tablelogback.domain.inquiry.dto.service.InquiryCreateServiceRequestDto;
 import com.tablelog.tablelogback.domain.inquiry.dto.service.InquiryReadResponseDto;
-import com.tablelog.tablelogback.domain.inquiry.dto.service.InquirySliceReadResponseDto;
 import com.tablelog.tablelogback.domain.inquiry.entity.Inquiry;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
